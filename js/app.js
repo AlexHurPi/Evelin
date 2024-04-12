1d8f285851fe018211c9f9c245d39f78734982a6
Alexander Hurtado <alexander.hurtado7170@gmail.com>
2024-04-12 12:54:57 -0500

    commit 12/04/2024 -7

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
+
+const nav = document.querySelector("#nav");
+const abrirMenu = document.querySelector("#abrirMenu");
+const cerrarMenu = document.querySelector("#cerrarMenu");
+
+abrirMenu.addEventListener("click", () =>{
+    nav.classList.add("visible");
+});
+
+cerrarMenu.addEventListener("click", () =>{
+    nav.classList.remove("visible");
+});
+
+const perfilEvelyn =document.querySelector("#perfilEvelyn")
+const quienesSomos =document.querySelector("#quienesSomos");
+
+quienesSomos.addEventListener("click", ()=>{
+    perfilEvelyn.classList.add("mostrarPerfil");
+});
+document.addEventListener('click', function(event){
+    if (event.target !== quienesSomos)
+        perfilEvelyn.classList.remove("mostrarPerfil");   
+});
+
+
+
 $(document).foundation()
+
+
+
+
+
+
 
 
