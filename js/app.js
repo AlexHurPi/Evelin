032acc058527637131b101f59db803ec52c0660d
Alexander Hurtado <alexander.hurtado7170@gmail.com>
2024-04-13 12:39:38 -0500

    commit 13/04/2024 -1

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+//-----------------poner visible y esconder el menu---------------
 const nav = document.querySelector("#nav");
 const abrirMenu = document.querySelector("#abrirMenu");
 const cerrarMenu = document.querySelector("#cerrarMenu");
@@ -10,7 +10,7 @@ abrirMenu.addEventListener("click", () =>{
 cerrarMenu.addEventListener("click", () =>{
     nav.classList.remove("visible");
 });
-
+//-----------------poner visible la tarjeta de presentacion
 const perfilEvelyn =document.querySelector("#perfilEvelyn");
 const quienesSomos =document.querySelector("#quienesSomos");
 const iconoQuienesSomos =document.querySelector("#iconoQuienesSomos")
@@ -23,14 +23,19 @@ quienesSomos.addEventListener("click", ()=>{
         perfilEvelyn.classList.remove("mostrarPerfil");
     }    
 });
+//-------------ocultar la tarjeta de presentacion cuando da clic en cualquier parte de la pantalla excepto en el boton para evitar el error de no cerrar ni abrir
 document.addEventListener('click', (event)=>{
     if (event.target !== quienesSomos && !quienesSomos.contains(event.target)){
         perfilEvelyn.classList.remove("mostrarPerfil");  
-    }
-         
+    }         
 });
 
-$(document).foundation()
+
+
+
+
+
+
 
 
 
